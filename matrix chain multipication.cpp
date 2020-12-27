d23e73f5210ab7d91007a76c230b2555bbf688f8
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2020-12-27 16:57:45 +0530

    Update matrix chain multipication.cpp

--- matrix chain multipication.cpp
+++ matrix chain multipication.cpp
@@ -1,1 +1,22 @@
-
+int MatrixChainOrder(int p[], int i, int j) 
+{ 
+    if (i == j) 
+        return 0; 
+    int k; 
+    int min = INT_MAX; 
+    int count; 
+ 
+    for (k = i; k < j; k++)  
+    { 
+        count = MatrixChainOrder(p, i, k) 
+                + MatrixChainOrder(p, k + 1, j) 
+                + p[i - 1] * p[k] * p[j]; 
+  
+        if (count < min) 
+            min = count; 
+    } 
+  
+    // Return minimum count 
+    return min; 
+} 
+  
