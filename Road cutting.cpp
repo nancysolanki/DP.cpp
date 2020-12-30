fdccef872f28c9f95e1697f35f2e0e4659348b7b
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2020-12-30 21:13:26 +0530

    Update Road cutting.cpp

--- Road cutting.cpp
+++ Road cutting.cpp
@@ -1,1 +1,18 @@
 
+ 
+#include<stdio.h> 
+#include<limits.h> 
+
+int max(int a, int b) { return (a > b)? a : b;} 
+int cutRod(int price[], int n) 
+{ 
+if (n <= 0) 
+	return 0; 
+int max_val = INT_MIN; 
+
+for (int i = 0; i<n; i++) 
+		max_val = max(max_val, price[i] + cutRod(price, n-i-1)); 
+
+return max_val; 
+} 
+
